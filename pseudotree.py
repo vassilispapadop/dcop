985a32e8e8d071a9c27ffcc18342b8ea0460d883
Vassilis <vassilispapadop@gmail.com>
2021-01-08 11:47:37 +0200

    refactor dpop.py created

--- pseudotree.py
+++ pseudotree.py
@@ -48,55 +48,6 @@ def getLeafNodes(tree):
         l['attributes'].print_node()
     return leaves
 
-def compute_utils(T, nodes):
-    # compute utility from each node and pass it to parents
-    # keep track of parents
-    parents = []
-    for node in nodes:
-        n_attributes = node['attributes']
-        print('')
-        print('----------------Computing utility for node: ', n_attributes.id, '----------------')
-        # n_attributes.print_node()
-
-        # find parent of current node
-        parent = getParent(T, n_attributes.id, pseudo=False)
-        if parent == None:
-            print('Node is root of tree, stop Util propagation and proceed with Value propagation')
-            n_attributes.print_node()
-            return
-
-        parents.append(parent)
-        p_attributes = parent['attributes']
-
-        # find common meetings between those two
-        commonMeetings = u.intersection(n_attributes.meetings, p_attributes.meetings)
-        for key in commonMeetings:
-            nodeUtility = n_attributes.meetings[key]
-            parentUtility = p_attributes.meetings[key]
-            # construct a SLOTS X SLOTS matrix
-            UTILMatrix = np.zeros(shape=(u.TIME_SLOTS, u.TIME_SLOTS))
-
-            for i in range(0,u.TIME_SLOTS):
-    	        for j in range(0,u.TIME_SLOTS):
-                    if i == j:
-                        UTILMatrix[i][j] = - 1
-                    else:
-                        UTILMatrix[i][j] = max(nodeUtility * n_attributes.preference[j], 
-                                                parentUtility * p_attributes.preference[j])
-            
-            # find per column maximum
-            MSG = {
-                    'childId': n_attributes.id, 
-                    'meetingId':key, 
-                    'util': np.array(np.max(UTILMatrix,axis=0))
-            }
-            #  update parent msg (send message to parent)
-            p_attributes.addUtilMsg(MSG)
-            p_attributes.print_node()
-            # print(UTILMatrix)
-
-    compute_utils(T,parents)
-
 def addNodes(G, agents):
     print ('----------------Adding nodes----------------')
     for agent in agents:
@@ -150,52 +101,3 @@ def addBackEdges(T, back_edges_candidates):
 
     return T
 
-def main():
-    # 1st row: Number of agents;Number of meetings;Number of variables
-
-    # Open file 
-    # inputFilename = 'dcop_constraint_graph'
-    inputFilename = 'dcop_simple'
-    # inputFilename = 'DCOP_Problem_40'
-    input = open(inputFilename, 'r') 
-
-    # Read first line
-    [nrAgents, nrMeetings, nrVars] = u.readLine(input)
-
-    # Read agents/variables/constraints
-    agents = u.readMeetings(input, nrVars)
-
-    # Read preferences per agent
-    agents = u.readPreferences(input, agents, nrAgents)
-
-    # Create graph
-    G = nx.Graph()
-    
-    # Add agents/nodes
-    G = addNodes(G, agents)
-
-    # Print nodes
-    u.printNodes(G)
-
-    # Add edges and keep track of back-edges
-    [G, back_edges_candidates] = addEdges(G, agents, nrMeetings)
-
-    # Convert to spanning tree
-    T = nx.maximum_spanning_tree(G)
-    # T = nx.dfs_successors(G, 0)
-
-    # Create back edges
-    T = addBackEdges(T, back_edges_candidates)
-
-    layout = graphviz_layout(T, prog="dot")
-
-    edges = T.edges()
-    colors = [T[u][v]['color'] for u,v in edges]
-    #print(list(nx.bfs_edges(T,3)))
-    compute_utils(T, getLeafNodes(T))
-
-    # nx.draw(T, layout, edge_color=colors, with_labels=True)
-    # plt.show()
-
-if __name__ == '__main__':
-    main()	
